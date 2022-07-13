8f89b220533ed89ad7e46348c87971b1498c247f
ApolloSolo <matt1teixeira@gmail.com>
2022-07-13 10:48:18 -0400

    Polished redirects - added logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import Auth from "../utils/auth.js"
+import Auth from "../utils/auth.js";
 
 const Navbar = () => {
   const [nav, setNav] = useState(true);
@@ -10,29 +10,36 @@ const Navbar = () => {
     setNav(!nav);
   };
 
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <nav className="text-[#ff2323] z-50 items-center h-20 flex justify-between border-[#ff0000] max-w-[1240px] mx-auto px-4 border-b-2 font-bold fixed left-0 right-0 top-0 bg-slate-800">
-      <h1 className="text-3xl ">Pokedex</h1>
+     <Link to={"/pokemon"}>
+     <h1 className="text-3xl hover:scale-105 duration-75">Pokedex</h1>
+     </Link>
       <ul className="hidden md:flex">
         <Link to={"/"}>
-          <li className="p-4">Home</li>
+          <li className="p-4 hover:scale-105 duration-75">Home</li>
         </Link>
         <Link to={"/pokemon"}>
-          <li className="p-4">All Mons</li>
+          <li className="p-4 hover:scale-105 duration-75">All Mons</li>
         </Link>
         {Auth.loggedIn() ? (
           <>
-           <li className="p-4">My Poke</li>
+            <li className="p-4 hover:scale-105 duration-75">My Poke</li>
+            <li onClick={logout} className="cursor-pointer p-4 hover:scale-105 duration-75">Log Out</li>
           </>
         ) : (
           <>
-          <Link to={"/login"}>
-          <li className="p-4">Log In</li>
-          </Link>
-          <li className="p-4">Register</li>
+            <Link to={"/login"}>
+              <li className="p-4 hover:scale-105 duration-75">Login</li>
+            </Link>
+            <li className="p-4 hover:scale-105 duration-75">Register</li>
           </>
         )}
-    
       </ul>
       <div onClick={handleNav} className="block md:hidden">
         {!nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
